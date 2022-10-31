2abd9dff5c8ee58093ef2377d9ff7192899ac0d1
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2022-10-31 09:43:57 -0300

    Nova exemplo sobre persistência de datas.

---
+++ java/Ex_04_JDBC_Datas/sql/script.sql
@@ -0,0 +1,14 @@
+drop database if exists exemplo_04_jdbc;
+create database exemplo_04_jdbc;
+
+use exemplo_04_jdbc;
+
+create table usuario (
+	id int primary key auto_increment not null,
+    nome varchar(200) not null,
+    pin int not null,
+    data_nascimento date not null,
+    data_acesso datetime not null
+);
+
+select * from usuario;
