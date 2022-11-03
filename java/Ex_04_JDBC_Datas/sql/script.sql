f1df26c111cd508aef3f6aeea7096341da853e52
Felipe A P Pinto <felipe.pinto@ifrn.edu.br>
2022-11-03 11:36:34 -0300

    Correção de alinhamento.

--- java/Ex_04_JDBC_Datas/sql/script.sql
+++ java/Ex_04_JDBC_Datas/sql/script.sql
@@ -4,7 +4,7 @@ create database exemplo_04_jdbc;
 use exemplo_04_jdbc;
 
 create table usuario (
-	id int primary key auto_increment not null,
+    id int primary key auto_increment not null,
     nome varchar(200) not null,
     pin int not null,
     data_nascimento date not null,
